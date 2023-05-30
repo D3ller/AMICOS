3eb2241a3ed35fb60bdfcdd0fa658fb6b38fb1c4
D3ller <corentin.nelhomme@gmail.com>
2023-05-30 20:39:37 +0200

    Ajout lib, inscrition et la verification

---
+++ assets/php/lib.php
@@ -0,0 +1,19 @@
+<?php
+
+function connect() {
+    $host_name = 'db5013211722.hosting-data.io';
+    $database = 'dbs11084435';
+    $user_name = 'dbu4744312';
+    $password = 'maconel0477';
+  
+    $conn = new mysqli($host_name, $user_name, $password, $database);
+  
+    if ($conn->connect_error) {
+      die("Échec de la connexion : " . $conn->connect_error);
+    }
+
+    return $conn;
+
+  }
+  
+?>
