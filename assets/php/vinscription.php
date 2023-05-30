5a1a263a9dc8f5a43c46e06b975e5c43d7e66966
D3ller <corentin.nelhomme@gmail.com>
2023-05-30 20:47:50 +0200

    Changement de la redirection vers la bonne page

--- assets/php/vinscription.php
+++ assets/php/vinscription.php
@@ -13,32 +13,32 @@ $groupe = $_POST['group'];
 
 if(empty($nom) || empty($prenom) || empty($email) || empty($password) || empty($password2) || empty($groupe)) {
 $_SESSION['error'] = "Veuillez remplir tous les champs";
-header('Location: ../inscription.php');
+header('Location: /inscription.php');
 }
 
 if($password != $password2){
     $_SESSION['error'] = "Les mots de passe ne sont pas identiques";
-    header('Location: ../inscription.php');
+    header('Location: /inscription.php');
 }
 
 if(strlen($password) < 8){
     $_SESSION['error'] = "Le mot de passe doit contenir au moins 8 caractères";
-    header('Location: ../inscription.php');
+    header('Location: /inscription.php');
 }
 
 if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
     $_SESSION['error'] = "L'email n'est pas valide";
-    header('Location: ../inscription.php');
+    header('Location: /inscription.php');
 }
 
 if(!preg_match('/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*\W).{8,}$/', $password)){
     $_SESSION['error'] = "Le mot de passe doit contenir au moins une lettre majuscule et un chiffre et un caractere special";
-    header('Location: ../inscription.php');
+    header('Location: /inscription.php');
 }
 
 if($groupe != 'A' && $groupe != 'B' && $groupe != 'C' && $groupe != 'D' && $groupe != 'E' && $groupe != 'F'){
     $_SESSION['error'] = "Le groupe n'est pas valide";
-    header('Location: ../inscription.php');
+    header('Location: /inscription.php');
 }
 
 //hash password
@@ -54,7 +54,7 @@ $result = $dbh->query($sql);
 
 if($result->num_rows > 0){
     $_SESSION['error'] = "L'email existe déjà";
-    header('Location: ../inscription.php');
+    header('Location: /inscription.php');
 }
 
 $description = "Je m\'apelle $prenom $nom et je suis dans le groupe $groupe";
