305c9a98aefdecac3110e677c5f99b395c6de534
D3ller <corentin.nelhomme@gmail.com>
2023-05-31 15:48:28 +0200

    Maps

--- index.php
+++ index.php
@@ -16,6 +16,7 @@ require_once('./assets/php/lib.php');
     <link rel="stylesheet" href="./assets/css/header-footer.css">
     <script src="./assets/js/script.js" DEEFER></script>
     <title>Accueil</title>
+    <script src="https://maps.googleapis.com/maps/api/js?libraries=places&callback=initAutocomplete&language=fr&output=json&key=AIzaSyCd8vcZ5809PqtE13gop5pdAKe2gRezwGo" async defer></script>
 </head>
 <body>
     <?php 
@@ -25,25 +26,88 @@ require_once('./assets/php/lib.php');
 
     <main>
 
-        
+        <?php
+
+        if(isset($_SESSION['AMIMAIL']) || isset($_SESSION['AMINAME'])){
+            // echo "<p>Vous êtes connecté</p>";
+
+            $dbh = connect();
+
+            $sql = "SELECT * FROM profil WHERE id = ? AND email = ?";
+            $stmt = $dbh->prepare($sql);
+            $stmt->bind_param("ss", $_SESSION['AMIID'], $_SESSION['AMIMAIL']);
+            $stmt->execute();
+            $user = $stmt->get_result()->fetch_assoc();    
+
+            echo "<p class='header-p-white'>Bienvenue ".$user['prenom']." <a href='./assets/php/deconnexion.php'>Déconnexion</a></p>";
+        } else {
+            // echo "<p>Vous n'êtes pas connecté</p>";
+            echo "<a class='info-con' href='./connexion.php'>Connexion</a>";
+            echo ' | ';
+            echo "<a class='info-con' href='./inscription.php'>Inscription</a>";
+            echo ' | ';
+            echo "<a class='info-con' href='./forget.php'>Mot de passe oublié</a>";
+        }
+        ?>
 
     </main>
 
-    <input type="text" value="Départ" onclick="">
-<input type="text" value="Arrivée" onclick="">
+
+<input type="text" id="address" placeholder="Départ">
+<input id='adress2' type="text" placeholder="Arrivée">
 <input type="datetime-local" value="Date" onclick="">
 <input type="submit" value="Rechercher">
 
-<iframe src="https://storage.googleapis.com/maps-solutions-xnfwwwbqef/address-selection/68ir/address-selection.html"
-  width="100%" height="400px"
-  style="border:0;"
-  loading="lazy">
-</iframe>
+
+<input type="hidden" id="lat" value="">
+<input type="hidden" id="lng" value="">
+
+<input type="hidden" id="lat2" value="">
+<input type="hidden" id="lng2" value="">
+
+<script type="text/javascript">
+  function initAutocomplete() {
+    var address = document.getElementById('address');
+    var autocomplete = new google.maps.places.Autocomplete(address);
+
+    var address2 = document.getElementById('adress2');
+    var autocomplete2 = new google.maps.places.Autocomplete(address2);
+
+    autocomplete.addListener('place_changed', function() {
+      var place = autocomplete.getPlace();
+      var latitude = place.geometry.location.lat();
+      var longitude = place.geometry.location.lng();
+      document.getElementById('lat').value = latitude;
+      document.getElementById('lng').value = longitude;
+    });
+
+    autocomplete2.addListener('place_changed', function() {
+      var place = autocomplete2.getPlace();
+      var latitude = place.geometry.location.lat();
+      var longitude = place.geometry.location.lng();
+      document.getElementById('lat2').value = latitude;
+      document.getElementById('lng2').value = longitude;
+    });
+
+  }
+
+   
+</script>
+
+
 
     
     <?php
     require_once 'footer.php';
     ?>
+
+    <script>
+
+
+
+
+
+</script>
     
 </body>
 </html>
