be6e2f559c421255c3ab27333f4d3ce271f636ae
D3ller <corentin.nelhomme@gmail.com>
2023-05-31 14:55:20 +0200

    Ajoute d'input

--- index.php
+++ index.php
@@ -21,7 +21,7 @@ require_once('./assets/php/lib.php');
     <?php 
     require_once 'header.php';
     ?>
-    cc
+    
 
     <main>
 
@@ -50,6 +50,12 @@ require_once('./assets/php/lib.php');
         ?>
 
     </main>
+
+    <input type="text" value="Départ" onclick="">
+<input type="text" value="Arrivée" onclick="">
+<input type="datetime-local" value="Date" onclick="">
+<input type="submit" value="Rechercher">
+
     
     <?php
     require_once 'footer.php';
