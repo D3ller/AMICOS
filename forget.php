134bc88153265fb5575f5e4cd6cd2f9532ace313
D3ller <corentin.nelhomme@gmail.com>
2023-05-31 11:10:15 +0200

    Enlever les erreurs

--- forget.php
+++ forget.php
@@ -4,6 +4,13 @@ session_start();
 
 require_once('./assets/php/lib.php');
 
+//dont display errors
+
+ini_set('display_errors', 0);
+ini_set('display_startup_errors', 0);
+error_reporting(E_ALL);
+
+
 $email = $_GET['mail'];
 $token = $_GET['token'];
 
