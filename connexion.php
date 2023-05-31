55976e54b4d7ab4385a7721fd100e334c9cfa317
MeriMinor <melvinbecue@gmail.com>
2023-05-31 15:06:52 +0200

    add css conexion et inscription

--- connexion.php
+++ connexion.php
@@ -21,34 +21,45 @@ if(isset($_SESSION['AMIMAIL']) || isset($_SESSION['AMINAME'])){
     <title>Connexion</title>
 </head>
 <body>
+    <?php 
+        require_once 'header.php';
+    ?>
 
-<?php
+    <main>
 
-if(isset($_SESSION['error'])){
-    echo $_SESSION['error'];
-    unset($_SESSION['error']);
-}
+        <?php
 
-?>
+        if(isset($_SESSION['error'])){
+            echo $_SESSION['error'];
+            unset($_SESSION['error']);
+        }
 
-    <form METHOD="POST" action="./assets/php/vconnexion.php">
-        <input type="mail" id='mail' name="email" placeholder="email">
-        <input type="password" name="password" placeholder="password">
-        <input type="submit" name="submit" value="connexion">
-    </form>
+        ?>
 
-<script>alert("Garde l'id de mot de pass oublié j'ai mis du js")</script>
+            <form METHOD="POST" action="./assets/php/vconnexion.php">
+                <input type="mail" id='mail' name="email" placeholder="email">
+                <input type="password" name="password" placeholder="password">
+                <input type="submit" name="submit" value="connexion">
+            </form>
 
-    <a href="./inscription.php">Inscription</a> | <a id='forget' href="./forget.php">Mot de passe oublié</a>
+        <script>alert("Garde l'id de mot de pass oublié j'ai mis du js")</script>
 
-    <script>
-var forget = document.getElementById('forget');
-var mail = document.getElementById('mail');
+            <a href="./inscription.php">Inscription</a> | <a id='forget' href="./forget.php">Mot de passe oublié</a>
 
-mail.addEventListener('input', function(){
-    forget.href = './forget.php?mail='+mail.value;
-})
-   </script>
+    </main>
+
+        <script>
+    var forget = document.getElementById('forget');
+    var mail = document.getElementById('mail');
+
+    mail.addEventListener('input', function(){
+        forget.href = './forget.php?mail='+mail.value;
+    })
+    </script>
+
+    <?php 
+    require_once 'footer.php';
+    ?>
 
 </body>
 </html>
--- inscription.php
+++ inscription.php
@@ -22,33 +22,42 @@ if(isset($_SESSION['AMIMAIL']) || isset($_SESSION['AMINAME'])){
     <title>Inscription</title>
 </head>
 <body>
+    <?php 
+        require_once 'header.php';
+    ?>
 
-<?php
-//verifie si y'a une erreur et si y'en a une affiche la mais tu peux deplacer cette partie n'importe ou dans le code
-if(isset($_SESSION['error'])){
-    echo $_SESSION['error'];
-    unset($_SESSION['error']);
-}
+    <main>
+        <?php
+        //verifie si y'a une erreur et si y'en a une affiche la mais tu peux deplacer cette partie n'importe ou dans le code
+        if(isset($_SESSION['error'])){
+            echo $_SESSION['error'];
+            unset($_SESSION['error']);
+        }
 
-?>
+        ?>
 
-    <form method="POST" action="./assets/php/vinscription.php">
-    <input type="text" name="nom" id="nom" placeholder="Nom">
-    <input type="text" name="prenom" id="prenom" placeholder="Prénom">
-    <input type="text" name="email" id="email" placeholder="Email">
-    <input type="text" name="password" id="password" placeholder="Mot de passe">
-    <input type="text" name="password2" id="password2" placeholder="Confirmer le mot de passe">
-    <select name="group" required>
-        <option value="">Groupe</option>
-        <option value="A">A</option>
-        <option value="B">B</option>
-        <option value="C">C</option>
-        <option value="D">D</option>
-        <option value="E">E</option>
-        <option value="F">F</option>
-    </select>
+            <form method="POST" action="./assets/php/vinscription.php">
+            <input type="text" name="nom" id="nom" placeholder="Nom">
+            <input type="text" name="prenom" id="prenom" placeholder="Prénom">
+            <input type="text" name="email" id="email" placeholder="Email">
+            <input type="text" name="password" id="password" placeholder="Mot de passe">
+            <input type="text" name="password2" id="password2" placeholder="Confirmer le mot de passe">
+            <select name="group" required>
+                <option value="">Groupe</option>
+                <option value="A">A</option>
+                <option value="B">B</option>
+                <option value="C">C</option>
+                <option value="D">D</option>
+                <option value="E">E</option>
+                <option value="F">F</option>
+            </select>
 
-    <input type="submit" value="S'inscrire">
-    </form>
+            <input type="submit" value="S'inscrire">
+            </form>
+    </main>
+
+    <?php 
+        require_once 'footer.php';
+    ?>
 </body>
 </html>
