6b8e0a362776e5a277a21e9575702759af4c64c0
MeriMinor <melvinbecue@gmail.com>
2023-05-31 14:09:18 +0200

    add css et js V2

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ require_once('./assets/php/lib.php');
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./assets/css/contacts.css">
+    <link rel="stylesheet" href="./assets/css/index.css">
     <script src="./assets/js/script.js" DEEFER></script>
     <title>Accueil</title>
 </head>
--- inscription.php
+++ inscription.php
@@ -16,7 +16,7 @@ if(isset($_SESSION['AMIMAIL']) || isset($_SESSION['AMINAME'])){
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./assets/css/contacts.css">
+    <link rel="stylesheet" href="./assets/css/inscription.css">
     <script src="./assets/js/script.js" DEEFER></script>
     <title>Inscription</title>
 </head>
