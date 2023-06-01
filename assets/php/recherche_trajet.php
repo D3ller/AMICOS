5e3ec54c0d70850d914b96950ac663c3e04b7695
D3ller <corentin.nelhomme@gmail.com>
2023-06-01 17:16:48 +0200

    recherche trajet

--- assets/php/recherche_trajet.php
+++ assets/php/recherche_trajet.php
@@ -6,9 +6,28 @@ require_once('./lib.php');
 
 $depart = $_POST['depart'];
 $arrivee = $_POST['arrivee'];
+$datetime = $_POST['datetime'];
 
-echo '<h1>Recherche de trajet entre'. $depart. ' '. $arrivee .'</h1>';
+$lat = $_POST['lat'];
+$lng = $_POST['lng'];
+$lat2 = $_POST['lat2'];
+$lng2 = $_POST['lng2'];
 
+echo '<h1>Recherche de trajet entre '. $depart. ' et '. $arrivee .'</h1>';
+
+$dbh = connect();
+
+$sql = "SELECT * FROM trajet date >= ?";
+$stmt = $dbh->prepare($sql);
+$stmt->bind_param("s", $datetime);
+$stmt->execute();
+
+$result = $stmt->get_result();
+
+while($trajet = $result->fetch_assoc()) {
+    echo $trajet['lieu_depart'].'<br>';
+
+}
 
 
 ?>
