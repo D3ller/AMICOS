a98bfe182dbedd189f86bf5c15e0017d3ccdfa96
MeriMinor <melvinbecue@gmail.com>
2023-06-04 16:18:56 +0200

    Mise en place du swipe

---
+++ assets/js/index.js
@@ -0,0 +1,211 @@
+function initAutocomplete() {
+    var address = document.getElementById('address');
+    var autocomplete = new google.maps.places.Autocomplete(address);
+
+    var address2 = document.getElementById('adress2');
+    var autocomplete2 = new google.maps.places.Autocomplete(address2);
+
+    autocomplete.addListener('place_changed', function() {
+      var place = autocomplete.getPlace();
+      var latitude = place.geometry.location.lat();
+      var longitude = place.geometry.location.lng();
+      document.getElementById('lat').value = latitude;
+      document.getElementById('lng').value = longitude;
+    });
+
+    autocomplete2.addListener('place_changed', function() {
+      var place = autocomplete2.getPlace();
+      var latitude = place.geometry.location.lat();
+      var longitude = place.geometry.location.lng();
+      document.getElementById('lat2').value = latitude;
+      document.getElementById('lng2').value = longitude;
+    });
+
+  }
+    // -----------------------------
+
+function calculateDistanceAndCO2() {
+  var lat1 = parseFloat(document.getElementById('lat').value);
+  var lng1 = parseFloat(document.getElementById('lng').value);
+  var lat2 = parseFloat(document.getElementById('lat2').value);
+  var lng2 = parseFloat(document.getElementById('lng2').value);
+
+
+
+  var origin = new google.maps.LatLng(lat1, lng1);
+  var destination = new google.maps.LatLng(lat2, lng2);
+
+  var service = new google.maps.DirectionsService();
+  var request = {
+    origin: origin,
+    destination: destination,
+    travelMode: google.maps.TravelMode.DRIVING
+  };
+
+  service.route(request, function(response, status) {
+    if (status === google.maps.DirectionsStatus.OK) {
+      var route = response.routes[0];
+      var distance = 0;
+      var duration = 0;
+
+      for (var i = 0; i < route.legs.length; i++) {
+        distance += route.legs[i].distance.value;
+        duration += route.legs[i].duration.value;
+      }
+
+      var distanceInKm = distance / 1000;
+      var durationInHours = duration / 3600;
+
+      var co2Emission = calculateCO2Emission(distanceInKm);
+      var round = function(value) {
+        return Math.round(value * 100) / 100;
+      };
+
+        document.getElementById('distance').innerHTML = "Distance : " + round(distanceInKm) + " km";
+
+        var minutes = durationInHours * 60;
+        var hours = Math.floor(minutes / 60);
+        var minutes = minutes - (hours * 60);
+        document.getElementById('duree').innerHTML = "Durée : " + hours + " heures et " + Math.round(minutes) + " minutes";
+        document.getElementById('c02').innerHTML = "CO2 : " + round(co2Emission) + " kg";
+        console.log(durationInHours);
+
+
+    } else {
+        document.getElementById('distance').innerHTML = "Le trajet n'a pas pu être calculé.";
+        console.log(status);
+    }
+  });
+}
+
+function calculateCO2Emission(distanceInKm) {
+  var co2Emission = distanceInKm * 150 / 1000;
+  return co2Emission;
+}
+
+// -----------------------------
+// SWIPE SCRIPT
+// -----------------------------
+
+"use strict";
+
+var tinderContainer = document.querySelector(".tinder");
+var allCards = document.querySelectorAll(".tinder--card");
+var nope = document.getElementById("nope");
+var love = document.getElementById("love");
+
+function initCards(card, index) {
+  var newCards = document.querySelectorAll(".tinder--card:not(.removed)");
+
+  newCards.forEach(function (card, index) {
+    card.style.zIndex = allCards.length - index;
+    card.style.transform =
+      "scale(" + (20 - index) / 20 + ") translateY(-" + 30 * index + "px)";
+    card.style.opacity = (10 - index) / 10;
+  });
+
+  tinderContainer.classList.add("loaded");
+}
+
+initCards();
+
+allCards.forEach(function (el) {
+  var hammertime = new Hammer(el);
+
+  hammertime.on("pan", function (event) {
+    el.classList.add("moving");
+  });
+
+  hammertime.on("pan", function (event) {
+    if (event.deltaX === 0) return;
+    if (event.center.x === 0 && event.center.y === 0) return;
+
+    tinderContainer.classList.toggle("tinder_love", event.deltaX > 0);
+    tinderContainer.classList.toggle("tinder_nope", event.deltaX < 0);
+
+    var xMulti = event.deltaX * 0.03;
+    var yMulti = event.deltaY / 80;
+    var rotate = xMulti * yMulti;
+
+    event.target.style.transform =
+      "translate(" +
+      event.deltaX +
+      "px, " +
+      event.deltaY +
+      "px) rotate(" +
+      rotate +
+      "deg)";
+  });
+
+  hammertime.on("panend", function (event) {
+    el.classList.remove("moving");
+    tinderContainer.classList.remove("tinder_love");
+    tinderContainer.classList.remove("tinder_nope");
+
+    var moveOutWidth = document.body.clientWidth;
+    var keep = Math.abs(event.deltaX) < 80 || Math.abs(event.velocityX) < 0.5;
+
+    event.target.classList.toggle("removed", !keep);
+
+    if (keep) {
+      event.target.style.transform = "";
+    } else {
+      var endX = Math.max(
+        Math.abs(event.velocityX) * moveOutWidth,
+        moveOutWidth
+      );
+      var toX = event.deltaX > 0 ? endX : -endX;
+      var endY = Math.abs(event.velocityY) * moveOutWidth;
+      var toY = event.deltaY > 0 ? endY : -endY;
+      var xMulti = event.deltaX * 0.03;
+      var yMulti = event.deltaY / 80;
+      var rotate = xMulti * yMulti;
+
+      event.target.style.transform =
+        "translate(" +
+        toX +
+        "px, " +
+        (toY + event.deltaY) +
+        "px) rotate(" +
+        rotate +
+        "deg)";
+      initCards();
+    }
+  });
+});
+
+function createButtonListener(love) {
+  return function (event) {
+    var cards = document.querySelectorAll(".tinder--card:not(.removed)");
+    var moveOutWidth = document.body.clientWidth * 1.5;
+
+    if (!cards.length) return false;
+
+    var card = cards[0];
+
+    card.classList.add("removed");
+
+    if (love) {
+      card.style.transform =
+        "translate(" + moveOutWidth + "px, -100px) rotate(-30deg)";
+    } else {
+      card.style.transform =
+        "translate(-" + moveOutWidth + "px, -100px) rotate(30deg)";
+    }
+
+    initCards();
+
+    event.preventDefault();
+  };
+}
+
+var nopeListener = createButtonListener(false);
+var loveListener = createButtonListener(true);
+
+nope.addEventListener("click", nopeListener);
+love.addEventListener("click", loveListener);
+
+
+// -----------------------------
+// Fin SWIPE SCRIPT
+// -----------------------------
--- index.php
+++ index.php
@@ -14,7 +14,8 @@ require_once('./assets/php/lib.php');
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./assets/css/index.css">
     <link rel="stylesheet" href="./assets/css/header-footer.css">
-    <script src="./assets/js/script.js" DEEFER></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="./assets/js/index.js" DEFER></script>
     <title>Accueil</title>
     <script src="https://maps.googleapis.com/maps/api/js?libraries=places&callback=initAutocomplete&language=fr&output=json&region=FR&key=AIzaSyCd8vcZ5809PqtE13gop5pdAKe2gRezwGo" async defer></script>
 <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCd8vcZ5809PqtE13gop5pdAKe2gRezwGo&libraries=places,geometry&region=FR"></script>
@@ -29,9 +30,6 @@ require_once('./assets/php/lib.php');
         unset($_SESSION['error']);
     }
     ?>
-
-
-    
 
     <main>
 
@@ -57,177 +55,143 @@ require_once('./assets/php/lib.php');
         }
         ?>
 
-    </main>
+        <form action="/assets/php/recherche_trajet.php" method="post" required>
+        <input type="text" name='depart' id="address" placeholder="Départ" required >
+        <input id='adress2' name='arrivee' type="text" placeholder="Arrivée" required>
+        <input type="datetime-local" name='datetime' value="Date" onclick="" required>
 
-<form action="/assets/php/recherche_trajet.php" method="post" required>
-<input type="text" name='depart' id="address" placeholder="Départ" required >
-<input id='adress2' name='arrivee' type="text" placeholder="Arrivée" required>
-<input type="datetime-local" name='datetime' value="Date" onclick="" required>
+        <input name='lat' type="hidden" id="lat" value="" required>
+        <input name='lng' type="hidden" id="lng" value="" required>
 
-<input name='lat' type="hidden" id="lat" value="" required>
-<input name='lng' type="hidden" id="lng" value="" required>
+        <input name='lat2' type="hidden" id="lat2" value="" required>
+        <input name='lng2' type="hidden" id="lng2" value="" required>
 
-<input name='lat2' type="hidden" id="lat2" value="" required>
-<input name='lng2' type="hidden" id="lng2" value="" required>
-
-<input type="submit" value="Rechercher">
-</form>
+        <input type="submit" value="Rechercher">
+        </form>
 
 
 
-<p id='c02'></p>
-<p id='distance'></p>
-<p id='duree'></p>
+        <p id='c02'></p>
+        <p id='distance'></p>
+        <p id='duree'></p>
 
-<button onclick="calculateDistanceAndCO2()">Calculer</button>
+        <button onclick="calculateDistanceAndCO2()">Calculer</button>
 
-<?php
+        <?php
 
-require_once './assets/php/lib.php';
+        require_once './assets/php/lib.php';
 
-$dbh = connect();
+        $dbh = connect();
 
-$sql = "SELECT t.*, COUNT(p.id) AS num_rows
-        FROM trajet t
-        LEFT JOIN passager p ON t.id = p.trajet_id
-        GROUP BY t.id
-        HAVING num_rows < t.place
-        ORDER BY RAND()
-        LIMIT 5";
+        $sql = "SELECT t.*, COUNT(p.id) AS num_rows
+                FROM trajet t
+                LEFT JOIN passager p ON t.id = p.trajet_id
+                GROUP BY t.id
+                HAVING num_rows < t.place
+                ORDER BY RAND()
+                LIMIT 5";
 
-$stmt = $dbh->prepare($sql);
-$stmt->execute();
-$result = $stmt->get_result();
+        $stmt = $dbh->prepare($sql);
+        $stmt->execute();
+        $result = $stmt->get_result();
 
-echo '<h2>Trajets récents</h2>';
-echo '<div id="trajets">';
+        echo '<h2>Trajets récents</h2>';
+        echo '<div id="trajets">';
 
-while ($row = $result->fetch_assoc()) {
-    echo '<div class="trajet">';
-    echo "<p>".$row['lieu_depart']."</p>";
-    echo "<p>".$row['lieu_arrivee']."</p>";
-    echo "<p>".$row['date']."</p>";
-    echo "<p>".$row['num_rows']."/".$row['place']."</p>";
-    echo '<a href="./reserv/'.$row['id'].'">Voir</a>';
-    echo '</div>';
-}
+        while ($row = $result->fetch_assoc()) {
+            echo '<div class="trajet">';
+            echo "<p>".$row['lieu_depart']."</p>";
+            echo "<p>".$row['lieu_arrivee']."</p>";
+            echo "<p>".$row['date']."</p>";
+            echo "<p>".$row['num_rows']."/".$row['place']."</p>";
+            echo '<a href="./reserv/'.$row['id'].'">Voir</a>';
+            echo '</div>';
+        }
 
-echo '</div>';
-?>
+        echo '</div>';
+        ?>
 
+        <!-- SWIPE -->
+        <div id="swipe">
+            <div class="tinder">
+                <div class="tinder--status">
+                <i class="fa fa-remove"></i>
+                <i class="fa fa-heart"></i>
+                </div>
+            
+                <div class="tinder--cards">
+                <div class="tinder--card">
+                    <img src="https://placeimg.com/600/300/people">
+                    <h3>Demo card 3</h3>
+                    <p>This is a demo for Tinder like swipe cards</p>
+                </div>
+                <div class="tinder--card">
+                    <img src="https://placeimg.com/600/300/people">
+                    <h3>Demo card 3</h3>
+                    <p>This is a demo for Tinder like swipe cards</p>
+                </div>
+                <div class="tinder--card">
+                    <img src="https://placeimg.com/600/300/people">
+                    <h3>Demo card 3</h3>
+                    <p>This is a demo for Tinder like swipe cards</p>
+                </div>
+                <div class="tinder--card">
+                    <img src="https://placeimg.com/600/300/people">
+                    <h3>Demo card 3</h3>
+                    <p>This is a demo for Tinder like swipe cards</p>
+                </div>
+                <div class="tinder--card">
+                    <img src="https://placeimg.com/600/300/people">
+                    <h3>Demo card 3</h3>
+                    <p>This is a demo for Tinder like swipe cards</p>
+                </div>
+                <div class="tinder--card">
+                    <img src="https://placeimg.com/600/300/people">
+                    <h3>Demo card 3</h3>
+                    <p>This is a demo for Tinder like swipe cards</p>
+                </div>
+                <div class="tinder--card">
+                    <img src="https://placeimg.com/600/300/people">
+                    <h3>Demo card 3</h3>
+                    <p>This is a demo for Tinder like swipe cards</p>
+                </div>
+            
+                <div class="tinder--card">
+                    <img src="https://placeimg.com/600/300/animals">
+                    <h3>Demo card 2</h3>
+                    <p>This is a demo for Tinder like swipe cards</p>
+                </div>
+                <div class="tinder--card">
+                    <img src="https://placeimg.com/600/300/nature">
+                    <h3>Demo card 3</h3>
+                    <p>This is a demo for Tinder like swipe cards</p>
+                </div>
+                <div class="tinder--card">
+                    <img src="https://placeimg.com/600/300/tech">
+                    <h3>Demo card 4</h3>
+                    <p></p>
+                </div>
+                <div class="tinder--card">
+                    <img src="https://placeimg.com/600/300/arch">
+                    <h3>Demo card 5</h3>
+                    <p>This is a demo for Tinder like swipe cards</p>
+                </div>
+                </div>
+            
+                <div class="tinder--buttons">
+                <button id="nope"><i class="fa fa-remove"></i></button>
+                <button id="love"><i class="fa fa-heart"></i></button>
+                </div>
+            </div>
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/hammer.js/2.0.8/hammer.min.js"></script>
+        </div>
 
-
-<script type="text/javascript">
-  function initAutocomplete() {
-    var address = document.getElementById('address');
-    var autocomplete = new google.maps.places.Autocomplete(address);
-
-    var address2 = document.getElementById('adress2');
-    var autocomplete2 = new google.maps.places.Autocomplete(address2);
-
-    autocomplete.addListener('place_changed', function() {
-      var place = autocomplete.getPlace();
-      var latitude = place.geometry.location.lat();
-      var longitude = place.geometry.location.lng();
-      document.getElementById('lat').value = latitude;
-      document.getElementById('lng').value = longitude;
-    });
-
-    autocomplete2.addListener('place_changed', function() {
-      var place = autocomplete2.getPlace();
-      var latitude = place.geometry.location.lat();
-      var longitude = place.geometry.location.lng();
-      document.getElementById('lat2').value = latitude;
-      document.getElementById('lng2').value = longitude;
-    });
-
-  }
-
-</script>
-
-
-<script>
-
-
-    
-
-function calculateDistanceAndCO2() {
-  var lat1 = parseFloat(document.getElementById('lat').value);
-  var lng1 = parseFloat(document.getElementById('lng').value);
-  var lat2 = parseFloat(document.getElementById('lat2').value);
-  var lng2 = parseFloat(document.getElementById('lng2').value);
-
-
-
-  var origin = new google.maps.LatLng(lat1, lng1);
-  var destination = new google.maps.LatLng(lat2, lng2);
-
-  var service = new google.maps.DirectionsService();
-  var request = {
-    origin: origin,
-    destination: destination,
-    travelMode: google.maps.TravelMode.DRIVING
-  };
-
-  service.route(request, function(response, status) {
-    if (status === google.maps.DirectionsStatus.OK) {
-      var route = response.routes[0];
-      var distance = 0;
-      var duration = 0;
-
-      for (var i = 0; i < route.legs.length; i++) {
-        distance += route.legs[i].distance.value;
-        duration += route.legs[i].duration.value;
-      }
-
-      var distanceInKm = distance / 1000;
-      var durationInHours = duration / 3600;
-
-      var co2Emission = calculateCO2Emission(distanceInKm);
-      var round = function(value) {
-        return Math.round(value * 100) / 100;
-      };
-
-        document.getElementById('distance').innerHTML = "Distance : " + round(distanceInKm) + " km";
-
-        var minutes = durationInHours * 60;
-        var hours = Math.floor(minutes / 60);
-        var minutes = minutes - (hours * 60);
-        document.getElementById('duree').innerHTML = "Durée : " + hours + " heures et " + Math.round(minutes) + " minutes";
-        document.getElementById('c02').innerHTML = "CO2 : " + round(co2Emission) + " kg";
-        console.log(durationInHours);
+</main>
 
 
-    } else {
-        document.getElementById('distance').innerHTML = "Le trajet n'a pas pu être calculé.";
-        console.log(status);
-    }
-  });
-}
-
-function calculateCO2Emission(distanceInKm) {
-  var co2Emission = distanceInKm * 150 / 1000;
-  return co2Emission;
-}
-
-
-
-</script>
-
-
-
-    
     <?php
     require_once 'footer.php';
     ?>
-
-    <script>
-
-
-
-
-
-</script>
     
 </body>
 </html>
