edd40637ffb927d2dceaa0c4b933cdd5836af752
MeriMinor <melvinbecue@gmail.com>
2023-06-06 15:05:42 +0200

    conn css

--- inscription.php
+++ inscription.php
@@ -17,18 +17,15 @@ if(isset($_SESSION['AMIMAIL']) || isset($_SESSION['AMIID'])){
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./assets/css/inscription.css">
+    <link rel="stylesheet" href="./assets/css/conexion.css">
     <link rel="stylesheet" href="./assets/css/header-footer.css">
     <script src="./assets/js/script.js" DEEFER></script>
     <title>Inscription</title>
 </head>
 <body>
-    <?php 
-        require_once 'header.php';
-    ?>
 
     <main>
-        <?php
+    <?php
         //verifie si y'a une erreur et si y'en a une affiche la mais tu peux deplacer cette partie n'importe ou dans le code
         if(isset($_SESSION['error'])){
             echo $_SESSION['error'];
@@ -37,27 +34,33 @@ if(isset($_SESSION['AMIMAIL']) || isset($_SESSION['AMIID'])){
 
         ?>
 
+        <div class="conn-logo">
+            
+        </div>
+        <div class="form-inscription">
             <form method="POST" action="./assets/php/vinscription.php">
-            <input type="text" name="nom" id="nom" placeholder="Nom">
-            <input type="text" name="prenom" id="prenom" placeholder="Prénom">
-            <input type="text" name="email" id="email" placeholder="Email">
-            <input type="password" name="password" id="password" placeholder="Mot de passe">
-            <input type="password" name="password2" id="password2" placeholder="Confirmer le mot de passe">
-            <select name="group" required>
-                <option value="">Groupe</option>
-                <option value="A">A</option>
-                <option value="B">B</option>
-                <option value="C">C</option>
-                <option value="D">D</option>
-                <option value="E">E</option>
-                <option value="F">F</option>
-            </select>
+                <input type="text" name="nom" id="nom" placeholder="Nom">
+                <input type="text" name="prenom" id="prenom" placeholder="Prénom">
+                <input type="text" name="email" id="email" placeholder="Email">
+                <input type="password" name="password" id="password" placeholder="Mot de passe">
+                <input type="password" name="password2" id="password2" placeholder="Confirmer le mot de passe">
+                <select name="group" required>
+                    <option value="">Groupe</option>
+                    <option value="A">A</option>
+                    <option value="B">B</option>
+                    <option value="C">C</option>
+                    <option value="D">D</option>
+                    <option value="E">E</option>
+                    <option value="F">F</option>
+                </select>
 
-            <input type="submit" value="S'inscrire">
+                <input type="submit" value="S'inscrire">
             </form>
+        </div>
     </main>
 
     <?php 
+        require_once 'menu.php';
         require_once 'footer.php';
     ?>
 </body>
