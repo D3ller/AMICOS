e1b438a18520b31dbbcfa475e30b4be56bd4b6a3
D3ller <corentin.nelhomme@gmail.com>
2023-06-07 10:42:12 +0200

    Profil + footer

--- footer.php
+++ footer.php
@@ -1,4 +1,19 @@
-<footer>
-    <a href='/mentions.php'>Mentions légales</a>
-</footer>
+<details class='legalfooter'>
+<summary>Mention légales<span class="marker"></span> </summary>
+    Something small enough to escape casual notice.
+</details>
 
+<details class='legalfooter'>
+<summary>Politique de Confidentialités<span class="marker"></span> </summary>
+    Something small enough to escape casual notice.
+</details>
+
+<details class='legalfooter'>
+<summary>Condition Générales<span class="marker"></span> </summary>
+    Something small enough to escape casual notice.
+</details>
+
+<details class='legalfooter'>
+<summary>Cookies<span class="marker"></span> </summary>
+    Something small enough to escape casual notice.
+</details>
