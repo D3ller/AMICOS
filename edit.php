83bf03a7a1481d2558b17c320b1a76dd103d0e80
D3ller <corentin.nelhomme@gmail.com>
2023-06-08 18:43:08 +0200

    Edit

--- edit.php
+++ edit.php
@@ -15,25 +15,38 @@ if(!isset($_SESSION['AMIMAIL']) || !isset($_SESSION['AMIID'])){
 <head>
   <link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.13/css/select2.min.css" rel="stylesheet" />
   <link href='/assets/css/header-footer.css' rel='stylesheet' type='text/css'>
+  <link type="text/css" rel="stylesheet" href="/assets/css/profil.css">
 </head>
 <body>
 
+<?php
+
+require_once('customnav.php');
+
+$dbh = connect();
+
+$sql = "SELECT * FROM profil WHERE id = ? AND email = ?";
+$stmt = $dbh->prepare($sql);
+$stmt->bind_param("ss", $_SESSION['AMIID'], $_SESSION['AMIMAIL']);
+$stmt->execute();
+$result = $stmt->get_result();
+$user = $result->fetch_assoc();
+?>
+
 <form action="./assets/php/edit.php" method="post" enctype="multipart/form-data">
 
-  <?php 
-  
-  $dbh = connect();
 
-    $sql = "SELECT * FROM profil WHERE id = ? AND email = ?";
-    $stmt = $dbh->prepare($sql);
-    $stmt->bind_param("ss", $_SESSION['AMIID'], $_SESSION['AMIMAIL']);
-    $stmt->execute();
-    $result = $stmt->get_result();
-    $user = $result->fetch_assoc();
+<div id="profil"><img id="profil-pic" src="https://portfolio.karibsen.fr/assets/img/pp/6481ee5fdec886.21951936.jpg" alt="Photo de profil"><div id="edit-name">    <input type="text" name="nom" value="<?php echo $user['nom']; ?>" required><a href="./edit.php"><div id="edit"></div></a></div><p class="grey">Homme 18 ans</p></div>
+
+
+  <?php 
+
+
+  
+
 
     ?>
     <input type="hidden" name="id" value="<?php echo $user['id']; ?>">
-    <input type="text" name="nom" value="<?php echo $user['nom']; ?>" required>
     <input type="text" name="prenom" value="<?php echo $user['prenom']; ?>" required>
     <input type="email" name="email" value="<?php echo $user['email']; ?>" required>
     <textarea type="text" name="description" required><?php echo $user['description']; ?></textarea>
