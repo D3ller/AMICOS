3af8d3fe2d07830e5eb61732461e34bfd3966d0a
MeriMinor <melvinbecue@gmail.com>
2023-06-08 16:20:38 +0200

    pdc & cgu

--- conditiongeneral.php
+++ conditiongeneral.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link type='text/css' rel='stylesheet' href='/assets/css/condition.css'>
+    <link type='text/css' rel='stylesheet' href='/assets/css/conditions.css'>
     <link type='text/css' rel='stylesheet' href='/assets/css/header-footer.css'>
-    <title>Document</title>
+    <title>Conditions générales</title>
 </head>
 <body>
 
@@ -18,51 +18,25 @@ require_once('customnav.php');
 
 <div id='conditions'>
 
-<h1>Conditions générales d'utilisation</h2>
+    <h1>Version applicable à compter du 7 juin 2023</h2>
 
-<h2>1. Introduction</h2>
+    <h2>Droit applicable</h2>
 
-<p>Les présentes conditions générales d'utilisation (CGU) énoncent les modalités d'utilisation du site web de covoiturage (ci-après dénommé "le Site") et régissent la relation entre l'utilisateur du Site (ci-après dénommé "l'Utilisateur") et l'exploitant du Site (ci-après dénommé "l'Exploitant"). En utilisant le Site, l'Utilisateur accepte d'être lié par ces CGU dans leur intégralité.
-</p>
+    <p>Le site Internet https://mmi22c01.sae202.ovh et les présentes conditions générales sont soumises au droit français et sont rédigés en français.
+    Pour tout litige, il est fait attribution exclusive de juridiction aux tribunaux compétents du ressort de Lyon. PARKS se réserve le droit de modifier les présentes mentions à tout moment. L’utilisateur s’engage à la consulter régulièrement.
+    </p>
 
-<h2>2. Description du service</h2>
-<p>Le Site permet aux Utilisateurs d'entrer en contact les uns avec les autres pour organiser des trajets en covoiturage. L'Exploitant facilite la mise en relation, mais n'est pas partie aux accords conclus entre les Utilisateurs. L'Exploitant ne garantit pas la disponibilité de covoitureurs ou de trajets spécifiques.
-</p>
-<h2>3. Inscription</h2>
-<p>L'Utilisateur doit s'inscrire sur le Site pour accéder à certaines fonctionnalités. L'inscription peut nécessiter la fourniture de certaines informations personnelles, qui seront traitées conformément à la politique de confidentialité du Site.
-</p>
-<h2>4. Obligations de l'Utilisateur</h2>
-<p>L'Utilisateur s'engage à fournir des informations exactes et à jour lors de l'inscription et à maintenir la confidentialité de ses identifiants de connexion. L'Utilisateur est responsable de toutes les activités effectuées sous son compte.
-</p>
+    <h2>Limitation de responsabilité</h2>
+    <p>Vous utilisez le site https://mmi22c01.sae202.ovh  sous votre seule et entière responsabilité. PARKS ne pourra être tenu pour responsable des dommages directs ou indirects, tels que, notamment, préjudice matériel, pertes de données ou de programme, préjudice financier, résultant de l’utilisation de ce site ou de sites qui lui sont liés.
+    </p>
 
-<h2>5. Utilisation du Site</h2>
-<p>L'Utilisateur s'engage à utiliser le Site conformément aux lois en vigueur et aux présentes CGU. Il s'interdit notamment de :<br>
-- Publier du contenu diffamatoire, obscène, offensant ou illégal.<br>
-- Utiliser le Site pour collecter des données personnelles d'autres Utilisateurs.<br>
-- Porter atteinte aux droits de propriété intellectuelle de tiers.
-- Tenter d'accéder aux systèmes informatiques du Site de manière non autorisée.
-</p>
+    <h2>Mise à jour</h2>
+    <p>PARKS se réserve le droit de modifier et de mettre à jour, sans préavis, les présentes mentions légales et tous les éléments, produits présentés sur le site. L’ensemble de ces modifications s’impose aux internautes qui doivent consulter les présentes Conditions Générales lors de chaque connexion.
+    </p>
 
-<h2>6. Responsabilité de l'Utilisateur</h2>
-<p>
-L'Utilisateur assume l'entière responsabilité de ses actions sur le Site. L'Exploitant décline toute responsabilité en cas de dommages directs ou indirects résultant de l'utilisation du Site, sauf en cas de faute intentionnelle ou de négligence grave de la part de l'Exploitant.
-</p>
-
-<h2>7. Propriété intellectuelle</h2>
-<p>L'ensemble des contenus présents sur le Site, tels que les textes, les graphiques, les logos, les images, les vidéos, les logiciels et les marques, sont la propriété de l'Exploitant ou de ses partenaires et sont protégés par les lois relatives à la propriété intellectuelle. Toute utilisation non autorisée de ces contenus est strictement interdite.
-</p>
-
-<h2>8. Modification du Site et des CGU</h2>
-<p>L'Exploitant se réserve le droit de modifier, suspendre ou interrompre le Site, ainsi que les présentes CGU, à tout moment et sans préavis. Les Utilisateurs seront informés des modifications substantielles des CGU.
-</p>
-<h2>9. Liens externes</h2>
-<p>Le Site peut contenir des liens vers des sites web tiers. L'Exploitant n'exerce aucun contrôle sur ces sites et décline toute responsabilité quant à leur contenu ou à leur fonctionnement.
-</p>
-<h2>10. Protection des données personnelles</h2>
-<p>La collecte et le traitement des données personnelles des Utilisateurs sont régis par la politique de confidentialité du Site, disponible sur le Site lui-même.
-</p>
-<h2>11. Dispositions diverses</h2>
-<p>Si une disposition des présentes CGU est jugée invalide ou inapplicable, les autres dispositions resteront en vigueur. Les présentes CGU sont régies par les lois du pays dans lequel l'Exploitant est établi.
-</p>
 </div>
+
+<?php
+require_once('menu.php');
+?>
 
---
+++ pdc.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link type='text/css' rel='stylesheet' href='/assets/css/conditions.css'>
+    <link type='text/css' rel='stylesheet' href='/assets/css/header-footer.css'>
+    <title>Politique de confidentialité</title>
+</head>
+<body>
+
+<?php
+require_once('customnav.php');
+?>
+
+<div id='conditions'>
+
+    <h2>Protection des données personnelles</h2>
+
+    <p>PARKS s’engage à respecter votre vie privée et à protéger les informations que vous lui communiquez. En particulier, les données personnelles collectées sur le site Internet https://mmi22c01.sae202.ovh sont destinées à l’usage de PARKS et de ses filiales. Elles sont confidentielles et traitées comme telles.
+    </p>
+    <p>
+Concernant les informations à caractère nominatif que vous seriez amenés à nous communiquer, vous bénéficiez d’un droit d’accès et de rectification conformément à la loi française Informatique et Liberté n° 78-17 du 6 janvier 1978. Vous pouvez exercer ce droit auprès de PARKS - Iut de Troyes, 9, Rue de Québec, CS90396, 10026 Troyes Cedex
+La collecte de ces informations est nécessaire pour répondre à vos demandes d’offres commerciales et vous adresser, le cas échéant, une lettre d’information électronique.
+    </p>
+    <p>
+Nous vous signalons qu’afin de vous proposer des produits et services toujours plus adaptés, certaines informations à caractère non personnel, relatives à votre activité sur ce site, seront automatiquement collectées. Ces informations sont destinées à PARKS et pourront également être utilisées dans le cadre d’opérations commerciales ou de marketing ou servir de base à des études et analyses.
+Ces informations ne seront en aucun cas communiquées à des tiers. Seul le personnel de PARKS ou celui des agences qui nous assistent pour la gestion de ces opérations ont accès aux données. Ces agences sont tenues de respecter la confidentialité des informations et de ne les utiliser que pour l’opération bien précise pour laquelle elles doivent intervenir.
+    </p>
+
+    <h2>Sécurité</h2>
+    <p>Nous prenons toutes les précautions utiles afin de préserver l’intégrité des données, leur confidentialité et empêcher toute communication à des tiers non autorisés.
+    </p>
+
+    <h2>Données personnelles</h2>
+    <p>PARKS utilise les données suivantes afin de garantir un suivi efficace et personnalisé de votre utilisation. Ces données ne sont utilisées que dans le cadre de votre expérience. Les données sont issues de votre inscription PARKS. Les données ne sont accessibles qu'aux personnes habilités et ayant une nécessité d'accéder à ces informations. Toutes les données facultatives peuvent être gérées par vos soins.
+    </p>
+    <p>Utilisateur :
+        <br>- Prénom
+        <br>- Nom
+        <br>- Genre
+        <br>- Age
+        <br>- Domicile
+        <br>- Véhicule
+        <br>- Langue</p>
+
+</div>
+
+<?php
+require_once('menu.php');
+?>
+
