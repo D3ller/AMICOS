3c85c2b789ed5324cb134c316734b88ee52e306d
MeriMinor <melvinbecue@gmail.com>
2023-06-08 10:27:16 +0200

    test

--- index.php
+++ index.php
@@ -84,9 +84,19 @@ if(isset($_SESSION['AMIMAIL']) || isset($_SESSION['AMIID'])){
                                 today: 'Aujourd\'hui',
                                 clear: 'Effacer',
                                 close: 'Fermer'
-                            }
-                            onSelect: function(date) {
-                                myDateInput.value = date.toLocaleDateString('fr-FR');
+                            },
+                            toString(date) {
+                                const day = date.getDate().toString().padStart(2, '0');
+                                const month = (date.getMonth() + 1).toString().padStart(2, '0');
+                                const year = date.getFullYear().toString();
+                                return `${day}/${month}/${year}`;
+                            },
+                            parse(dateString) {
+                                const parts = dateString.split('/');
+                                const day = parseInt(parts[0], 10);
+                                const month = parseInt(parts[1], 10) - 1;
+                                const year = parseInt(parts[2], 10);
+                                return new Date(year, month, day);
                             }
                         });
                     </script>
